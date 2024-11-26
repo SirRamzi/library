8bb09e082321a48ba6f5ef2516c157b1105a4c17
Прокофьев Александр <aleks.aleksp@gmail.com>
2024-11-26 14:13:07 +0400

    Added Hibernate annotations in Book model

--- src/main/java/ru/prokofev/library/models/Book.java
+++ src/main/java/ru/prokofev/library/models/Book.java
@@ -1,18 +1,42 @@
 package ru.prokofev.library.models;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 
+import java.util.Date;
+
+@Entity
+@Table(name = "book")
 public class Book {
 
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(name = "name")
     @NotEmpty(message = "Название должно быть указано")
     private String name;
+
+    @Column(name = "author")
     @NotEmpty(message = "Автор должен быть указано")
     private String author;
+
+    @Column(name = "year")
     @Min(value = 1, message = "Год должен быть больше 0")
     private int year;
+
+    @Column(name = "date_last_capture")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dateLastCapture;
+
+    @Column(name = "person_id")
     private Integer person_id;
+
+    @ManyToOne
+    @JoinColumn(name = "person_id", referencedColumnName = "id", insertable=false, updatable=false)
+    private Person person;
 
     public int getId() {
         return id;
@@ -46,11 +70,27 @@ public class Book {
         this.year = year;
     }
 
+    public Date getDateLastCapture() {
+        return dateLastCapture;
+    }
+
+    public void setDateLastCapture(Date dateLastCapture) {
+        this.dateLastCapture = dateLastCapture;
+    }
+
     public Integer getPerson_id() {
         return person_id;
     }
 
     public void setPerson_id(Integer person_id) {
         this.person_id = person_id;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
     }
 }
