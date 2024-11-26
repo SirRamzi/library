db22f6fe9841263ea87133ec4c93448ce3603d58
Прокофьев Александр <aleks.aleksp@gmail.com>
2024-11-27 00:05:48 +0400

    Added BookRepository

---
+++ src/main/java/ru/prokofev/library/repositories/BookRepository.java
@@ -0,0 +1,9 @@
+package ru.prokofev.library.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.prokofev.library.models.Book;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Integer> {
+}
