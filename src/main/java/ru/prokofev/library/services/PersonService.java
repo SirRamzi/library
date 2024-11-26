45208fd057f2a9c82a84b330c4d6ccfd17d061ba
Прокофьев Александр <aleks.aleksp@gmail.com>
2024-11-27 00:20:27 +0400

    Added method for getting people in PersonService

--- src/main/java/ru/prokofev/library/services/PersonService.java
+++ src/main/java/ru/prokofev/library/services/PersonService.java
@@ -1,7 +1,23 @@
 package ru.prokofev.library.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.prokofev.library.models.Person;
+import ru.prokofev.library.repositories.PersonRepository;
+
+import java.util.List;
 
 @Service
 public class PersonService {
+
+    private final PersonRepository personRepository;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    public List<Person> getPeople() {
+        return personRepository.findAll();
+    }
 }
